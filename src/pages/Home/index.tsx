c442617a3e3e059bcdd62a15ce50d7ad047e595f
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-03-15 16:41:39 -0300

    feat: :sparkles: add a loader spinner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+import { CenteredDiv } from "./styles";
+
 import { useNavigate } from "react-router-dom";
 
 import { Header } from "@components";
+
+import CircularProgress from "@mui/material/CircularProgress";
 
 import getCarsData from "@services";
 import { setData } from "@redux/carsSlice";
 import { useDispatch, useSelector } from "react-redux";
 
+import Colors from "@/shared/constants/colors";
+
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    getCarsData().then((response) => console.log(response));
+    setIsLoading(true);
+    getCarsData().then((response) => {
+      setIsLoading(false);
+      console.log(response);
+    });
   }, []);
 
   return (
     <div>
       <Header />
-      <button onClick={() => navigate("/details")}>Clique</button>
+      {!isLoading ? (
+        <CenteredDiv>Hi</CenteredDiv>
+      ) : (
+        <CenteredDiv>
+          <CircularProgress color="primary" />
+        </CenteredDiv>
+      )}
     </div>
   );
 };
